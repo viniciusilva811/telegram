c8a1abdd55945c2fdb06b505e6f9612e34767c65
Vinicius <vinisilva45@live.com>
2020-11-09 15:42:11 -0300

    Configurando o envio de mensagem.

---
+++ dialogflow.js
@@ -0,0 +1,23 @@
+const dialogflow = require('dialogflow');
+const configs = require('./dio-bot.json');
+
+const sessionClient = new dialogflow.SessionsClient({
+    projectId: configs.project_id,
+    credentials: {
+        private_key: configs.private_key,
+        client_email: configs.client_email
+    }
+});
+
+async function sendMessage(chatId, message){
+    const sessionPath = sessionClient.sessionPath(configs.project_id, chatId);
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text: message,
+                languageCode: 'pt-BR'
+            }
+        }
+    }
+}
