146d63b70985c91f20b0cffcf1432e46f03256d2
Vinicius <vinisilva45@live.com>
2020-11-09 14:04:47 -0300

    Alteração em um erro.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const token = '1394297392:AAF_IQ7ioB5zfReIoYfoWEwQvOuLkvx1hSI';
 const bot = new TelegramBot(token, {polling: true});
 
 bot.on('message', function(msg){
-    const chatId = msg.chat.chatId;
+    const chatId = msg.chat.id;
     console.log(msg.text);
     bot.sendMessage(chatId, 'Obrigado...');
 });
